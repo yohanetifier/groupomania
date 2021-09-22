a6dcc7e0a56f4be59c71961cedf7110f06156fed
Yohan Etifier <etifier.y@gmail.com>
2021-09-22 21:22:21 +0200

    end function like with heart

--- backend/controllers/like.js
+++ backend/controllers/like.js
@@ -1,26 +1,36 @@
 const Post = require('../models/Post')
 const Like = require('../models/Like')
 
-exports.getlike = (req, res, next) => {
-    if(req.body.like === 1){
-        const like = Like.build({
-            post_id: req.params.post_id,
-            user_id: req.body.userId,
-        }) 
-        like.save()
-        .then(() => res.status(201).json({message: "Like added"}))
-        .catch((error) => res.status(404).json({error}))
-    }else{
-        Like.findOne({
-            where : {post_id: req.params.post_id, user_id: req.body.userId}
-        })
-        .then((like) => {
-            if(like){
-                like.destroy()
-            res.status(200).json({message: "Like canceled"})
-            }else {
-                res.status(400).json({message: "No like in database"})
-            }
-        })
+exports.createlike = (req, res, next) => {
+  Like.findOne({
+    where: {
+      post_id: req.params.post_id,
+      user_id: req.body.userId,
+    },
+  })
+  .then((like) => {
+    if (like) {
+        like.destroy({
+            where: { 
+                post_id: req.params.id, 
+                user_id: req.body.userId
+        }})
+        res.status(200).json({message: "Like canceled"})
+    } else {
+      const like = Like.build({
+        post_id: req.params.post_id,
+        user_id: req.body.userId,
+      })
+      like
+        .save()
+        .then(() => res.status(201).json({ message: 'Like added' }))
+        .catch((error) => res.status(404).json({ error }))
     }
+  })
+}
+
+exports.getAllLikeByPost = (req, res, next ) => {
+    Like.findAll()
+    .then((like) => res.status(200).json(like))
+    .catch((error ) => res.status(404).json({error}))
 }
--- backend/routes/like.js
+++ backend/routes/like.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router()
 const likeCtrl = require('../controllers/like')
 
-router.post('/:post_id', likeCtrl.getlike)
+router.post('/:post_id', likeCtrl.createlike)
+router.get('/bypost', likeCtrl.getAllLikeByPost)
 
 module.exports = router
